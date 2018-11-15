775692b70c1abbfd4bc5f9a836e4dcfa49c0c13d
Jack <jb17017@my.bristol.ac.uk>
2018-11-15 15:39:06 +0000

    Initial commit for spring-view-start

---
+++ src/main/java/BristolArchives/controllers/BristolArchivesRunController.java
@@ -0,0 +1,13 @@
+package BristolArchives.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@Controller
+public class BristolArchivesRunController {
+        @GetMapping("/")
+        public String index() {
+            return "index";
+        }
+}

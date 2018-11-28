c9ee22bdbb934bb26183e3af1e8d566176b46b47
Andrew Page <andrew.page.2017@my.bristol.ac.uk>
2018-11-28 20:27:37 +0000

    Realised that dictionary keys must be unique, so must have only one location column. Regenerate csv files!

--- src/main/sql/collections.sql
+++ src/main/sql/collections.sql
@@ -14,7 +14,7 @@ CREATE TABLE SubCollection (
   subCollectionRef  VARCHAR(100)   NOT NULL,
   -- Maybe omit this, since relatively few will have names
   -- name              VARCHAR(100)   NULL,
-  description       TEXT           NOT NULL,
+  description       TEXT           NULL,
   
   collectionId      INTEGER        NOT NULL,
   FOREIGN KEY    (collectionId) REFERENCES Collection(id)
@@ -23,15 +23,16 @@ CREATE TABLE SubCollection (
 CREATE TABLE Item (
   id             		INTEGER        PRIMARY KEY AUTO_INCREMENT,
   itemRef                       VARCHAR(100)   NOT NULL,
-  name           		VARCHAR(100)   NOT NULL,
-  description           	TEXT           NOT NULL,
+  location                      VARCHAR(100)   NOT NULL,
+  name           		VARCHAR(100)   NULL,
+  description           	TEXT           NULL,
   -- For MVP, date will be a single string, due to inconsistent data entry
   -- dateAdded		 	DATE	       NULL,
   -- earliestDateCreated        DATE	       NULL,
   -- latestDateCreated		DATE	       NULL,
   dateCreated                   VARCHAR(100)   NOT NULL,
   
-  copyrighted                   BIT            NOT NULL,
+  copyrighted                   VARCHAR(100)   NOT NULL,
   extent			VARCHAR(100)   NOT NULL,
   physTechDesc			TEXT           NULL,
   

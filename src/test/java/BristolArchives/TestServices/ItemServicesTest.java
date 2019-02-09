bf67bd4e3d5ea22fe9f73a1362d8c2a2c914ecc4
Evangeline <zy17864@my.bristol.ac.uk>
2019-02-09 12:12:57 +0000

    Commit before changing branch

--- src/test/java/BristolArchives/TestServices/ItemServicesTest.java
+++ src/test/java/BristolArchives/TestServices/ItemServicesTest.java
@@ -1,10 +1,37 @@
 package BristolArchives.TestServices;
 
+import BristolArchives.entities.Item;
+import BristolArchives.entities.SubCollection;
+import BristolArchives.services.ItemService;
+import org.junit.Before;
+import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.mockito.Mockito;
+import org.mockito.stubbing.Answer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.List;
+
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@DataJpaTest
 public class ItemServicesTest {
+    @Autowired
+    ItemService itemService;
+
+    @Before
+    public void setUp() {
+        Item testItem = new Item("01/2019","testItem","testLocation","test Description","06/02/2019","false","testExten","testPhysDesc","testMulIrn",testSubColl);
+        Answer<Item> ans = new A;
+        Mockito.when(itemService.getItemByName(testItem.getName())).thenReturn(List.asList(testItem));
+    }
+
+    @Test
+    public void whenFindByName_thenReturnItem() {
+        SubCollection testSubColl = new SubCollection();
+        testSubColl.setId(1);
+        testSubColl.setName("testSubColl");
+                //assertThat(itemService.getItemByName("testItem").size().isEqualTo(1));
+    }
 }

7780de12600a78276b46f08a8f755e3378de5c69
Evangeline <zy17864@my.bristol.ac.uk>
2019-02-09 12:43:14 +0000

    A quick fix on syntax withuot thinking what the statement means ._.

--- src/main/java/BristolArchives/repositories/ItemRepo.java
+++ src/main/java/BristolArchives/repositories/ItemRepo.java
@@ -29,10 +29,10 @@ public interface ItemRepo extends JpaRepository<Item,Integer>{
     @Query("select i from item i where i.itemRef = CONCAT(:search,'')")
     List<Item> findWithRef(@Param("search")String search);
 
-    @Query("select i from item i where i.start_date = :specificdate or :specificdate between i.start_date and i.end_date")
+    @Query("select i from item i where i.startDate = CONCAT(:specificdate,'') or CONCAT(:specificdate,'') between i.startDate and i.endDate")
     List<Item> findBySpecificDate(@Param("specificdate")String specificdate);
 
-    @Query("select i from item i where not (i.start_date > :start or i.end_date < :end)")
+    @Query("select i from item i where not (i.startDate > CONCAT(:start,'') or i.endDate < CONCAT(:end,''))")
     List<Item> findByDateRange(@Param("start")String start, @Param("end")String end);
 
 }

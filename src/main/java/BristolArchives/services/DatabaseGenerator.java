dcdd27df385c6d44270dfe893b17143218a74ca8
Andrew Page <andrew.page.2017@my.bristol.ac.uk>
2019-02-22 13:09:03 +0000

    Began writing DatabaseGenerator

---
+++ src/main/java/BristolArchives/services/DatabaseGenerator.java
@@ -0,0 +1,38 @@
+package BristolArchives.services;
+
+/*
+This class should load up a CSV file with a clearly-specified set of columns
+containing the required data fields about archive/museum items. It should go
+through them creating entity instances (Dept, Collection and Item) and actually
+insert them into the MySQL database that the program is connected to. It may have
+to do so in batches, firstly to limit memory usage, and secondly to minimize the
+number of SQL queries necessary, thereby increasing performance.
+
+This class should therefore depend on the Repo classes, and a CSV-reading library.
+
+The set of data fields needed for an Item is as follows:
+
+Object number
+Location (of provenance/production)
+Name
+Description
+Date (we will infer/extract start_date and end_date)
+Copyright status/holder
+Extent
+Phys/Tech/Desc
+Multimedia IRN
+Collection display name (e.g. Trotter)
+Collection id (just an SQL auto-generated integer: a foreign key into collection table)
+
+For a Collection, we just need its name and its Dept id (again a integer foreign key).
+For a Dept, we just need its name.
+
+In order to insert an Item, we first need to insert its Collection, and to do that,
+we first need to insert that Collection's Dept. So the order is Dept, Collection, Item.
+Need to keep track of those that have already been added.
+
+ */
+
+public class DatabaseGenerator {
+
+}

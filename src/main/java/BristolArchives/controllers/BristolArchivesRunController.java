35046c60cbcaf5e874a567c89eb720f5e9602ab6
Andrew Page <andrew.page.2017@my.bristol.ac.uk>
2019-02-22 14:24:43 +0000

    Trying to check if it is working, but nothing is printed to console

--- src/main/java/BristolArchives/controllers/BristolArchivesRunController.java
+++ src/main/java/BristolArchives/controllers/BristolArchivesRunController.java
@@ -1,13 +1,23 @@
 package BristolArchives.controllers;
 
+import BristolArchives.services.DatabaseGenerator;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.io.File;
 
 
 @Controller
 public class BristolArchivesRunController {
     @GetMapping("/")
     public String index() {
+        // Why is nothing being printed to console???
+        System.out.println("\n\nHELLOASKJDHFKAJSDHFLJASDLFJHASKFHLKASDJFHLKAFH\n\n");
+        DatabaseGenerator dbGen = new DatabaseGenerator();
+        File file = dbGen.getCSVFile("haslam.csv");
+        System.out.println(file.getAbsolutePath());
+        dbGen.generateDatabase(file);
+        System.out.println("\n\nHELLOASKJDHFKAJSDHFLJASDLFJHASKFHLKASDJFHLKAFH\n\n");
         return "index";
     }
 }

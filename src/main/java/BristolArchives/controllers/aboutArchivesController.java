fdedc1c865db1c9b22dccf78d4e2ba89def71f80
Jack <jb17017@my.bristol.ac.uk>
2019-03-19 11:19:55 +0000

    added a html page for basic login, if u need the form uncomment it and add the correct controller link, controller currently in aboutArchivesController for testing

---
+++ src/main/java/BristolArchives/controllers/aboutArchivesController.java
@@ -0,0 +1,17 @@
+package BristolArchives.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class aboutArchivesController {
+    @GetMapping("/aboutArchives")
+    public String aboutArchives(){
+        return "aboutArchives";
+    }
+    @GetMapping("/loginPage")
+    public String loginPage(){
+        return "loginPage";
+    }
+}
+

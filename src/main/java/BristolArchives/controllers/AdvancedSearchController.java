2c1b94f58ce1e2d28514138861f868e04690e476
Evangeline <zy17864@my.bristol.ac.uk>
2019-03-22 10:33:54 +0000

    Improved advanced search

--- src/main/java/BristolArchives/controllers/AdvancedSearchController.java
+++ src/main/java/BristolArchives/controllers/AdvancedSearchController.java
@@ -26,17 +26,10 @@ import java.util.stream.IntStream;
 public class AdvancedSearchController {
     @Autowired
     private ItemService itemService;
-    private CollectionService collectionService;
+//    private CollectionService collectionService;
 
     private boolean hasSth(String s) {
         return (s != null) && (!s.isEmpty());
-    }
-
-    private void getIntersection(List<Item> result, List<Item> newItems, boolean someConstraintsExist) {
-        if(result.isEmpty() && !someConstraintsExist)
-            result.addAll(newItems);
-        else
-            result.retainAll(newItems);
     }
 
     @GetMapping("/advancedSearch")
@@ -67,10 +60,6 @@ public class AdvancedSearchController {
             @RequestParam("page") Optional<Integer> page,
             @RequestParam("size") Optional<Integer> size
         ){
-
-        //System.out.printf("collection: %s, single_date: %s, start: %s, end: %s, whole_phrase: %s, location: %s",
-        //        adv_coll, checkForNull(adv_date), checkForNull(adv_date_start), checkForNull(adv_date_end), adv_name, adv_lctn);
-
         String search = "?";
 
         if(hasSth(coll))
@@ -91,37 +80,38 @@ public class AdvancedSearchController {
         return "redirect:/advSearch" + search + "&page=" + page.orElse(1) + "&size=" + size.orElse(5);
     }
 
-    private String searchString(@RequestParam(value = "collection_search", required = false) String adv_coll,
-                         @RequestParam(value = "date_search", required = false) String adv_date,
-                         @RequestParam(value = "date_start", required = false) String adv_date_start,
-                         @RequestParam(value = "date_end", required = false) String adv_date_end,
-                         @RequestParam(value = "precision_search", required = false) String adv_name,
-                         @RequestParam(value = "location_search", required = false) String adv_lctn){
-        String search = "?";
-
-        if(hasSth(adv_coll))
-            search += "&coll=" + adv_coll;
-        if(hasSth(adv_date_start))
-            search += "&dateStart=" + adv_date_start;
-        if(hasSth(adv_date_end))
-            search += "&dateEnd=" + adv_date_end;
-        if(hasSth(adv_date))
-            search += "&date=" + adv_date;
-        if(hasSth(adv_name))
-            search += "&name=" + adv_name;
-        if(hasSth(adv_lctn))
-            search += "&lctn=" + adv_lctn;
-        search = search.replaceAll("/","%2F");
-        return search;
-    }
-
     private Date parseDate(String str) {
+        if(str == "")
+            return null;
+
         DateFormat df = new SimpleDateFormat("yyyy-mm-dd");
         try {
             return df.parse(str);
         }
         catch (ParseException e) {
             return new Date();
+        }
+    }
+
+    private void displayPageNumber(Model model, Page<Item> itemPage, int currentPage) {
+        int totalPages = itemPage.getTotalPages();
+        List<Integer> pageNumbers;
+        currentPage = Math.min(totalPages, currentPage);
+
+        if (totalPages > 0) {
+            int startPage, endPage;
+
+            if(totalPages <= 5) {
+                startPage = 1;
+                endPage = totalPages;
+            } else {
+                startPage = Math.max(currentPage - 2, 1);
+                endPage = Math.max(5, Math.min(currentPage + 2, totalPages));
+            }
+            pageNumbers = IntStream.rangeClosed(startPage, endPage)
+                    .boxed()
+                    .collect(Collectors.toList());
+            model.addAttribute("pageNumbers", pageNumbers);
         }
     }
 
@@ -141,173 +131,120 @@ public class AdvancedSearchController {
         if(!hasSth(coll) && !hasSth(date) && !hasSth(name) && !hasSth(lctn) && !hasSth(date_start) && !hasSth(date_end))
             return "redirect:/advancedSearch";
 
-        if (hasSth(date)) {
-            System.out.println("advanced date");
-            /*model.addAttribute("itemList",
-                    itemService.getAdvancedSearch(
-                            parseDate(adv_date),
-                            null,
-                            null,
-                            adv_coll,
-                            adv_lctn,
-                            adv_name));*/
-            int currentPage = page.orElse(1);
-            int pageSize = size.orElse(5);
+        int currentPage = page.orElse(1);
+        int pageSize = size.orElse(5);
 
-            Page<Item> itemPage = itemService.findPaginatedAdvSearch(parseDate(date),
-                    null,
-                    null,
-                    coll,
-                    lctn,
-                    name, PageRequest.of(currentPage - 1, pageSize));
+        date = hasSth(date) ? date : "";
+        date_start = hasSth(date_start) ? date_start : "";
+        date_end = hasSth(date_end) ? date_end : "";
 
-            model.addAttribute("itemPage", itemPage);
-            model.addAttribute("specificDate", date);
-            model.addAttribute("startDate", "");
-            model.addAttribute("endDate", "");
-            model.addAttribute("collection", coll);
-            model.addAttribute("location", lctn);
-            model.addAttribute("precision", name);
+//        System.out.println(date);
+//        System.out.println(date_start);
+//        System.out.println(date_end);
+//        System.out.println();
+
+        Page<Item> itemPage = itemService.findPaginatedAdvSearch(
+                parseDate(date),
+                parseDate(date_start),
+                parseDate(date_end),
+                coll,
+                lctn,
+                name,
+                PageRequest.of(Math.max(currentPage - 1, 0), Math.max(pageSize, 1))
+        );
+
+        model.addAttribute("itemPage", itemPage);
+        model.addAttribute("specificDate", date);
+        model.addAttribute("startDate", date_start);
+        model.addAttribute("endDate", date_end);
+        model.addAttribute("collection", coll);
+        model.addAttribute("location", lctn);
+        model.addAttribute("precision", name);
+
+        displayPageNumber(model, itemPage, currentPage);
 
 
-
-            int totalPages = itemPage.getTotalPages();
-            List<Integer> pageNumbers = new ArrayList();
-            if (totalPages > 0) {
-                if (currentPage >= 3 && currentPage <= totalPages - 2) {
-                    pageNumbers = IntStream.rangeClosed(currentPage - 2, Math.min(currentPage + 2, totalPages))
-                            .boxed()
-                            .collect(Collectors.toList());
-                }
-                else if(currentPage < 3){
-                    pageNumbers = IntStream.rangeClosed(1, Math.min(5,totalPages))
-                            .boxed()
-                            .collect(Collectors.toList());
-                }
-                else if(currentPage > totalPages - 2){
-                    pageNumbers = IntStream.rangeClosed(totalPages - 5, totalPages)
-                            .boxed()
-                            .collect(Collectors.toList());
-                }
-                model.addAttribute("pageNumbers", pageNumbers);
-            }
-        }
-        if(hasSth(date_start) && hasSth(date_end)) {
-           /* model.addAttribute("itemList",
-                    itemService.getAdvancedSearch(
-                            null,
-                            parseDate(adv_date_start),
-                            parseDate(adv_date_end),
-                            adv_coll,
-                            adv_lctn,
-                            adv_name));*/
-            int currentPage = page.orElse(1);
-            int pageSize = size.orElse(5);
-
-            Page<Item> itemPage = itemService.findPaginatedAdvSearch(null, parseDate(date_start),
-                    parseDate(date_end),
-                    coll,
-                    lctn,
-                    name, PageRequest.of(currentPage - 1, pageSize));
-
-            model.addAttribute("itemPage", itemPage);
-            model.addAttribute("specificDate", "");
-            model.addAttribute("startDate", date_start);
-            model.addAttribute("endDate", date_end);
-            model.addAttribute("collection", coll);
-            model.addAttribute("location", lctn);
-            model.addAttribute("precision", name);
-
-            int totalPages = itemPage.getTotalPages();
-            List<Integer> pageNumbers = new ArrayList();
-            if (totalPages > 0) {
-                if (currentPage >= 3 && currentPage <= totalPages - 2) {
-                    pageNumbers = IntStream.rangeClosed(currentPage - 2, Math.min(currentPage + 2, totalPages))
-                            .boxed()
-                            .collect(Collectors.toList());
-                }
-                else if(currentPage < 3){
-                    pageNumbers = IntStream.rangeClosed(1, Math.min(5,totalPages))
-                            .boxed()
-                            .collect(Collectors.toList());
-                }
-                else if(currentPage > totalPages - 2){
-                    pageNumbers = IntStream.rangeClosed(totalPages - 5, totalPages)
-                            .boxed()
-                            .collect(Collectors.toList());
-                }
-                model.addAttribute("pageNumbers", pageNumbers);
-            }
-            /*
-            boolean someConstraintsExist = false;
-            List<Item> resultList = new ArrayList();
-            if(adv_name != null) {
-                getIntersection(resultList, itemService.getItemByName(adv_name), false);
-                someConstraintsExist = true;
-            }
-            if(adv_date != null) {
-                getIntersection(resultList,itemService.getItemByDate(adv_date), someConstraintsExist);
-                someConstraintsExist = true;
-            }
-            if(adv_coll != null) {
-                getIntersection(resultList, itemService.getItemByCollectionName(adv_coll), someConstraintsExist);
-                someConstraintsExist = true;
-            }
-            if(adv_lctn != null)
-                getIntersection(resultList, itemService.getItemByLocation(adv_lctn), someConstraintsExist);
-            model.addAttribute("itemList", resultList);
-            */
-        }
-        if(!hasSth(date) && (!hasSth(date_start) || !hasSth(date_end))){
-            /*model.addAttribute("itemList",
-                    itemService.getAdvancedSearch(
-                            null,
-                            null,
-                            null,
-                            adv_coll,
-                            adv_lctn,
-                            adv_name));*/
-            int currentPage = page.orElse(1);
-            int pageSize = size.orElse(5);
-
-            Page<Item> itemPage = itemService.findPaginatedAdvSearch(null,
-                    null,
-                    null,
-                    coll,
-                    lctn,
-                    name, PageRequest.of(currentPage - 1, pageSize));
-
-            model.addAttribute("itemPage", itemPage);
-            model.addAttribute("specificDate", "");
-            model.addAttribute("startDate", "");
-            model.addAttribute("endDate", "");
-            model.addAttribute("collection", coll);
-            model.addAttribute("location", lctn);
-            model.addAttribute("precision", name);
-
-            int totalPages = itemPage.getTotalPages();
-            List<Integer> pageNumbers = new ArrayList();
-            if (totalPages > 0) {
-                if (currentPage >= 3 && currentPage <= totalPages - 2) {
-                    pageNumbers = IntStream.rangeClosed(currentPage - 2, Math.min(currentPage + 2, totalPages))
-                            .boxed()
-                            .collect(Collectors.toList());
-                }
-                else if(currentPage < 3){
-                    pageNumbers = IntStream.rangeClosed(1, Math.min(5,totalPages))
-                            .boxed()
-                            .collect(Collectors.toList());
-                }
-                else if(currentPage > totalPages - 2){
-                    pageNumbers = IntStream.rangeClosed(totalPages - 5, totalPages)
-                            .boxed()
-                            .collect(Collectors.toList());
-                }
-                model.addAttribute("pageNumbers", pageNumbers);
-            }
-
-        }
-
+//        if (hasSth(date)) {
+//            System.out.println("advanced date");
+//            /*model.addAttribute("itemList",
+//                    itemService.getAdvancedSearch(
+//                            parseDate(adv_date),
+//                            null,
+//                            null,
+//                            adv_coll,
+//                            adv_lctn,
+//                            adv_name));*/
+//
+//            Page<Item> itemPage = itemService.findPaginatedAdvSearch(parseDate(date),
+//                    null,
+//                    null,
+//                    coll,
+//                    lctn,
+//                    name, PageRequest.of(currentPage - 1, pageSize));
+//
+//            model.addAttribute("itemPage", itemPage);
+//            model.addAttribute("specificDate", date);
+//            model.addAttribute("startDate", "");
+//            model.addAttribute("endDate", "");
+//            model.addAttribute("collection", coll);
+//            model.addAttribute("location", lctn);
+//            model.addAttribute("precision", name);
+//
+//            displayPageNumber(model, itemPage, currentPage);
+//        }
+//        if(hasSth(date_start) && hasSth(date_end)) {
+//           /* model.addAttribute("itemList",
+//                    itemService.getAdvancedSearch(
+//                            null,
+//                            parseDate(adv_date_start),
+//                            parseDate(adv_date_end),
+//                            adv_coll,
+//                            adv_lctn,
+//                            adv_name));*/
+//
+//            Page<Item> itemPage = itemService.findPaginatedAdvSearch(null, parseDate(date_start),
+//                    parseDate(date_end),
+//                    coll,
+//                    lctn,
+//                    name, PageRequest.of(currentPage - 1, pageSize));
+//
+//            model.addAttribute("itemPage", itemPage);
+//            model.addAttribute("specificDate", "");
+//            model.addAttribute("startDate", date_start);
+//            model.addAttribute("endDate", date_end);
+//            model.addAttribute("collection", coll);
+//            model.addAttribute("location", lctn);
+//            model.addAttribute("precision", name);
+//
+//            displayPageNumber(model, itemPage, currentPage);
+//        }
+//        if(!hasSth(date) && (!hasSth(date_start) || !hasSth(date_end))){
+//            /*model.addAttribute("itemList",
+//                    itemService.getAdvancedSearch(
+//                            null,
+//                            null,
+//                            null,
+//                            adv_coll,
+//                            adv_lctn,
+//                            adv_name));*/
+//
+//            Page<Item> itemPage = itemService.findPaginatedAdvSearch(null,
+//                    null,
+//                    null,
+//                    coll,
+//                    lctn,
+//                    name, PageRequest.of(currentPage - 1, pageSize));
+//
+//            model.addAttribute("itemPage", itemPage);
+//            model.addAttribute("specificDate", "");
+//            model.addAttribute("startDate", "");
+//            model.addAttribute("endDate", "");
+//            model.addAttribute("collection", coll);
+//            model.addAttribute("location", lctn);
+//            model.addAttribute("precision", name);
+//
+//            displayPageNumber(model, itemPage, currentPage);
+//        }
 
         return "advSearchResults";
     }
